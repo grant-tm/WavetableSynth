b3e832407c6830b1edda958a2251ca0f2aec6352
grant-tm <grant.t.morgan@gmail.com>
2024-07-10 23:03:32 -0700

    create static sine oscillator

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -114,6 +114,9 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+
+    SineOscillator sineOscillator;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (WavetableSynthAudioProcessor)
 };
